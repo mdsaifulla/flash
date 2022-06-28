3b3487b44370a15505c97e25da3c130f557a79c8
saiful <0073saiful@gmail.com>
2022-06-29 00:12:23 +0530

    second commit

---
+++ airline/src/airline/Collection.java
@@ -0,0 +1,21 @@
+package airline;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Collection extends Frame 
+{
+	Collection()
+	{
+		addWindowListener(new W());
+	}
+	class W extends WindowAdapter
+	{
+		public void windowClosing(WindowEvent e)
+		{
+			setVisible(false);
+			dispose();
+			
+		}
+	}
+}
+
---
+++ airline/src/airline/Waiting.java
@@ -0,0 +1,28 @@
+package airline;
+
+
+	
+	import java.awt.*;
+	import java.awt.event.*;
+
+	public class Waiting extends Frame 
+	{
+		Waiting()
+		{
+			addWindowListener(new W());
+		}
+		class W extends WindowAdapter
+		{
+			public void windowClosing(WindowEvent e)
+			{
+				setVisible(false);
+				//dispose();
+				System.exit(0);
+				
+			}
+		}
+	
+
+
+
+}
